567ca3aeb2f62a49acdb833b85510c518bbd313d
Dario Arias <h.dario95@gmail.com>
2017-03-07 11:45:30 -0500

    Home

--- ProyectoTWJ/AplicacionLibreria/src/app/home/home.component.ts
+++ ProyectoTWJ/AplicacionLibreria/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MasterURlService} from "../services/master-url.service";
-import {Http, Response} from "@angular/http";
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-home',
@@ -8,20 +8,32 @@ import {Http, Response} from "@angular/http";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  libros=[];
-  constructor(private _http: Http,
-              private _masterURL: MasterURlService) { }
+  private rutas:string[];
+  libros:any[];
 
+  constructor(private _http:Http,private _masterURL:MasterURlService) {
+    this.rutas=['assets/images/1.jpg','assets/images/2.png','assets/images/3.png','assets/images/4.jpg','assets/images/5.jpg']
+  }
   ngOnInit() {
-    this._http.get(this._masterURL.url + "Libro")
-      .subscribe(
-        (res: Response) => {
-          this.libros = res.json();
-        },
-        (err) => {
-          console.log(err);
-        }
-      )
+    this.cargarLibros();
   }
 
+  cargarLibros(){
+    this._http.get(this._masterURL.url+"libro")
+      .subscribe(
+        (res)=>{
+          this.libros=res.json().map((value) => {
+            value.ruta = this.ruta;
+            return value;
+          });
+        },
+        (err)=>{
+          alert("Ocurrio un error"+err.toString());
+        },
+      );
+  }
+
+  get ruta():string{
+    return this.rutas[Math.floor(Math.random()*this.rutas.length)];
+  }
 }
