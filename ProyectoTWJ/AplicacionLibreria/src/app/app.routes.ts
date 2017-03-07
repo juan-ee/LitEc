9136f5bbd20947aa9cad3747a6dda8bd7d869055
Dario Arias <h.dario95@gmail.com>
2017-03-07 02:04:38 -0500

    Autor

--- ProyectoTWJ/AplicacionLibreria/src/app/app.routes.ts
+++ ProyectoTWJ/AplicacionLibreria/src/app/app.routes.ts
@@ -8,7 +8,7 @@ export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'autor', component: AutorComponent},
-  {path: 'libro', component: LibroComponent}
+  {path: 'libros', component: LibroComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
---
+++ ProyectoTWJ/AplicacionLibreria/src/assets/bootstrap-3.3.7-dist/codigo.js
@@ -0,0 +1,20 @@
+function setInputDate(_id){
+    var _dat = document.querySelector(_id);
+  var hoy = new Date();
+
+    var d = hoy.getDate(),
+        m = hoy.getMonth()+1,
+        y = hoy.getFullYear(),
+        data;
+
+    if(d < 10){
+        d = "0"+d;
+    }
+    if(m < 10){
+        m = "0"+m;
+    }
+
+    data = y+"-"+m+"-"+(d);
+    _dat.value = data;
+}
+setInputDate("#datePicker");
